1ddea32a1d42d07121799de608b0e2f09881af1d
Hector Alvarado <hectoralvaradocampos83@gmail.com>
2021-11-09 20:32:53 -0300

    adding usuarios microservices

---
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/Role.java
@@ -0,0 +1,30 @@
+package com.formacionbdi.springboot.app.usuarios.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 30)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/Usuario.java
@@ -0,0 +1,76 @@
+package com.formacionbdi.springboot.app.usuarios.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, length = 20)
+    private String username;
+
+    private String password;
+    private Boolean enabled;
+    private String nombre;
+    private String apellido;
+    @Column(unique = true)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
